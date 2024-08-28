0b88ffb1a96b8b8f95d6b2401481bbf131ce5972
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2024-08-28 12:11:18 +0330

    add author list and detail view

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from library.views import AuthorCreateView
+from library.views import AuthorCreateView, AuthorListView, AuthorDetailView
 
 urlpatterns = [
+    # Author Model Urls
     path('author/create/', AuthorCreateView.as_view(), name='author-create'),
+    path('author/', AuthorListView.as_view(), name='author-list'),
+    path('author/<int:author_id>', AuthorDetailView.as_view(), name='author-detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -5,8 +5,22 @@ from .forms import AuthorForm
 from .models import Author
 
 
+# Author Model Views
 class AuthorCreateView(generic.CreateView):
     form_class = AuthorForm
     model = Author
     template_name = 'library/author-create.html'
     success_url = reverse_lazy('home')
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    template_name = 'library/author-list.html'
+    context_object_name = 'authors'
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    pk_url_kwarg = 'author_id'
+    template_name = 'library/author-detail.html'
+    context_object_name = 'author'
